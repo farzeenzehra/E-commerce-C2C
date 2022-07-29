a5844a2308ade2df6a70aa04ac477d6a2ebb4575
Farzeen Zehra <farzeenzehra.fz@gmail.com>
2022-07-29 21:59:46 +0500

    changed app local

--- app_local.js
+++ app_local.js
@@ -3,9 +3,8 @@ const abi = require('./contract_abi.json')
 const Web3 = require("web3");
 const ContractAddress = '0xaF03DE639f90a7F973C758A5eA5C9a3b0579f488';
 const walletAddOwner = '0xdF8e844F1BA70EC7466229380b80eb66F245Aa85';
-const walletAdd1 = '0x4FD2C724fE0bc55CD3de38f32b219D9eA83e3AFc';
-const walletAdd2 = '0xC237303710a8DFe883f1CF642e60A20c77428C0E';
-const walletAdd3 = '0x02302C1906193E671b186bff91b93CEE28223dA2';
+
+// number of wallet address defines concurrent users
 const wallets = ['0x4FD2C724fE0bc55CD3de38f32b219D9eA83e3AFc', 
                 '0xC237303710a8DFe883f1CF642e60A20c77428C0E', 
                 '0x02302C1906193E671b186bff91b93CEE28223dA2']
@@ -19,93 +18,112 @@ web3.eth.getBlockNumber().then((result) => {
 
 const contract = new web3.eth.Contract(abi, ContractAddress);
 
+// define all contract functions here
+
+// setter functions
 let addSoldProduct = (prodID, modelID, modelName, walletAdd)=>
                         contract.methods.addSoldProduct(prodID,modelID,modelName).send({from:walletAdd, gas:3000000})
-                        .then(r=>console.log(r.from)).catch(e=>console.log(e.message))
+                        .then(r=>console.log("Transaction hash:",r.transactionHash,"Business Wallet:",r.from)).catch(e=>console.log(e.message))
+
 let addBuiness = (walletAdd)=>contract.methods.addBuiness(walletAdd).send({from:walletAdd, gas:3000000})
-                        .then(r=>console.log(r.from)).catch(e=>console.log(e.message))
+                        .then(r=>console.log("Transaction hash:",r.transactionHash,"Business Wallet:",r.from)).catch(e=>console.log(e.message))
 
+let addModel = (modelID, modelName, brandName,category, walletAdd) => 
+        contract.methods.addModel(modelID, modelName, brandName, category).send({from:walletAdd, gas:3000000})
+        .then(r=>console.log("Transaction hash:",r.transactionHash,"Business Wallet:",r.from)).catch(e=>console.log(e.message))
 
+let transferOwnership = (prodID, hashedCNIC, walletAdd)=>
+        contract.methods.transferOwnership(prodID, hashedCNIC).send({from:walletAdd, gas:3000000})
+        .then(r=>console.log("Transaction hash:",r.transactionHash,"Business Wallet:",r.from)).catch(e=>console.log(e.message))
 
+// getter functions
+let getSoldProduct = (prodID, walletAdd) => 
+        contract.methods.getSoldProduct(prodID).call({from:walletAdd, gas:3000000})
+        .then(r=>console.log("Result:",r)).catch(e=>console.log(e.message))
+
+let getModel = (modelID, walletAdd) =>
+        contract.methods.getModel(modelID).call({from:walletAdd, gas:3000000})
+        .then(r=>console.log("Result:",r)).catch(e=>console.log(e.message))
+
+let getBusiness = (walletAdd) =>
+        contract.methods.getBusiness(walletAdd).call({from:walletAdd, gas:3000000})
+        .then(r=>console.log("Result:",r)).catch(e=>console.log(e.message))
+
+let getCustCNIC = (prodID, walletAdd) =>
+        contract.methods.getCustCNIC(prodID).call({from:walletAdd, gas:3000000})
+        .then(r=>console.log("Result:",r)).catch(e=>console.log(e.message))
+
+// to check balance of ethers in wallet
 // web3.eth.getBalance(walletAdd).then(console.log);
 
-// contract.methods.getCustCNIC("1").call().then(console.log)
-// add business
-// contract.methods.addBuiness(walletAdd3).send({from:walletAdd1,gas: 3000000  })
-//     .then((receipt)=>{
-//         console.log("Transaction hash:",receipt.transactionHash);
-//         console.log("Block hash:", receipt.blockHash);
-//         console.log("Gas Used:", receipt.gasUsed);
-//     })
-//     .catch(e=>console.log(Object.values(e.data)[0].reason))
-
-function runAddBusiness(runTimes){
-    let tasks = [];
-    for(i=0;i<runTimes;i++){
-        tasks.push(
-        contract.methods.addBuiness(walletAdd3).send({from:walletAdd1,gas: 3000000  })
-        .then((receipt)=>{
-            console.log("Transaction hash:",receipt.transactionHash);
-            console.log("Block hash:", receipt.blockHash);
-            console.log("Gas Used:", receipt.gasUsed);
-        })
-        .catch(e=>console.log(Object.values(e.data)[0].reason)))
-    }
-    
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let wallet of wallets)
-// console.log(wallet)
-
-let spcont = (contractMethod)=>contractMethod.then(r=>console.log(r.from)).catch(e=>console.log(e.message))
-async function getCNIC(){
+async function runConcurrentWithSameAttrs(runTimes, func, attrs){
     console.log("running...")
     let tasks = []
-    
-    for(i=0;i<10;i++){
+    for(i=0;i<runTimes;i++){
         for (let walletNo in wallets){
-            // tasks.push(contract.methods.getCustCNIC("1").call({from:wallet}).then((r)=>console.log(r)));
             tasks.push(
-                spcont(contract.methods.addSoldProduct(prodID,modelID,modelName).send({from:wallets[walletNo],gas: 3000000  }))
+                func(...attrs,wallets[walletNo])
             );
         }
     }
     await Promise.all(tasks);
 }
 
-async function concurrent(){
+async function sameAttrPrintTime(howManyTimesTorun, func, attrs){
     console.time('codezup')
-    // await Promise.all([getCNIC(walletAdd1,100),getCNIC(walletAdd2,200),getCNIC(walletAdd3,300)]);
-    await getCNIC();
+    await runConcurrentWithSameAttrs(howManyTimesTorun, func, attrs);
     console.timeEnd('codezup')
 }
-// contract.methods.addSoldProduct(`far-1`,"1","xyz").send({from:walletAdd1, gas:3000000}).then(r=>{
-//     console.log(r.from);
-// }).catch(e=>console.log(e));
-concurrent();
-// for(i=300;i<600;i++)
-// contract.methods.getCustCNIC("far-100").call({from:walletAdd1},i).then(console.log)
-// contract.methods.getCustCNIC("far-102").call({from:walletAdd1},615).then(console.log)
+
+async function diffAttrPrintTime(howManyTimesTorun, func)
+{
+    console.time('codezup')
+    await runConcurrentWithDiffAttrs(howManyTimesTorun, func);
+    console.timeEnd('codezup')
+}
+
+// change attributes as you want to use
+async function runConcurrentWithDiffAttrs(runTimes, func){
+    console.log("running...")
+    let tasks = []
+    for(i=0;i<runTimes;i++){
+        for (let walletNo in wallets){
+            tasks.push(
+                // define attributes of function in the bracket
+                func("far-"+walletNo+"-"+i
+                    , wallets[walletNo])
+            );
+        }
+    }
+    await Promise.all(tasks);
+}
+
+// get Previous data
+function getHistory(startBlockNo)
+{
+    web3.eth.getBlockNumber().then((endBlockNumber) => {
+        for(blockNo=startBlockNo; blockNo<=endBlockNumber;blockNo++){
+            // define contract function
+            contract.methods.getCustCNIC("1").call({from:wallets[0], gas:3000000}, blockNo)
+
+            .then(r=>{if(r?.length>0) console.log(r)})
+            .catch(e=>console.log(e.message));
+        }
+    });
+    
+    
+}
+
+// call all functions with same attributes
+sameAttrPrintTime(10, transferOwnership, ["1", "xyz"]);
+
+// call function with different atttributes then needs to be defined in runConcurrentWithDiffAttrs function
+// diffAttrPrintTime(10, transferOwnership)
+
+// change function in getHistory function
+// getHistory(617);
+
 
 
